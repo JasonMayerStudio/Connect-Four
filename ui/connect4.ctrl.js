7006d53fe848664ea63e5ec257b22b2f23da04de
Jason Mayer Studio <jason@jasonmayer.com>
2017-03-21 18:52:29 -0400

    shell application

---
+++ ui/connect4.ctrl.js
@@ -0,0 +1,14 @@
+(function() {
+    'use strict';
+
+    angular.module('connect4.controller', []).controller('connect4Controller',
+       searchController);
+
+
+    function connect4Controller($rootScope, $scope, $state) {
+        console.log('Inside the search controller');
+       
+    }
+    
+
+})();
