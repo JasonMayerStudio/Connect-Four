65cc5a5c0eb3da29002c24b818ba1aa4a40162e2
Jason Mayer Studio <jason@jasonmayer.com>
2017-03-28 10:27:03 -0400

    connect 4 service

---
+++ service/connect4-ui.srvc.js
@@ -0,0 +1,40 @@
+angular.module('c4Utils', [])
+.service('UiService', function() {
+
+        this.NOT_EMPTY = function() {
+            return this.value != undefined && this.value != '';
+        };
+
+        this.newField = function newField(defaultValue, validateCallback) {
+            return {
+                hasError: false,
+                value: defaultValue,
+                validate: validateCallback
+            };
+        };
+
+        this.checkErrors = function(fields) {
+
+            var result = true;
+
+            for(var i in fields) {
+
+                fields[i].hasError = false;
+
+                if(fields[i].validate != undefined && !fields[i].validate()) {
+                    fields[i].hasError = true;
+                    result = false;
+                }
+            }
+
+            return result;
+        };
+
+        this.buildRouteWithParams = function(path, params) {
+            var route = path + '/';
+            for(var i in params) {
+                route += params[i] + '/';
+            }
+            return route;
+        };
+});
